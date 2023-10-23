65690dc6ca70a2709b879f795441134e2684a445
Rick <rick15176@gmail.com>
2023-10-23 17:43:18 +0530

    initial commit: released jokes api project

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import bodyParser from "body-parser"
+import axios from "axios"
+
+const app = express();
+const port = 5000;
+const Api_URL = "https://v2.jokeapi.dev/joke/"
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res)=> {
+    res.render("index.ejs", {
+        content: "waiting for content...",
+    });
+
+});
+
+app.post("/get-joke", async (req, res)=> {
+    
+    const searchId = req.body.type;
+    console.log(searchId);
+    try {
+        const result = await axios.get(Api_URL + searchId + "?type=single"); 
+        console.log(result.data);
+        res.render("index.ejs", {
+            content: result.data,
+        });  
+    } 
+    catch (error) {
+        res.send(error.message);
+    }
+});
+
+app.listen(port, ()=> { 
+    console.log(`Server is started on port ${port}`);
+})
